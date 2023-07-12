7d881ce17719930624fae20bb6b6a57e65a72f56
abdiasiis78 <gureyxalane78@gmail.com>
2023-07-12 19:35:13 +0300

    dashboard-tenent-jsx

---
+++ src/pages/TenateDhashboard/Dhashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Nav from '../../components/TenDhashContent/Nav'
+import Side from '../../components/TenDhashContent/Side'
+
+function Dhashboard() {
+  return (
+    <div className='flex bg-[#f6f6f6]'>
+    <Side/>
+    <Nav/>
+    <div className='mt-[70px] h-100vh w-full p-11'>
+        <div className='flex flex-col'>
+            <h3 className='font-bold text-xl text-[#00befe]'>Dashboard</h3>
+            <span className="text-[#7E7E7E]">Welcome to TenantCare</span>
+        </div>
+        {/* content */}
+    </div>
+</div>
+  )
+}
+
+export default Dhashboard
