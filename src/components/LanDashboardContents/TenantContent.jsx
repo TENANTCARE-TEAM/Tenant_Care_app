d4b68876ddf20965dcb96bc251e7cf5c9bb65adb
unknown <masudahmed.edu@gmail.com>
2023-07-14 17:08:25 +0300

    tenant update

--- src/components/LanDashboardContents/TenantContent.jsx
+++ src/components/LanDashboardContents/TenantContent.jsx
@@ -13,7 +13,7 @@ function TenantContent() {
       <div className='mt-5 bg-white p-6 w-full flex flex-col shadow rounded'>
     <div className='mt-4 flex flex-wrap gap-5 p-4 pb-5'>
       <div className='flex max-[768px]:flex-col gap-6  overflow-hidden w-[480px] h-auto max-[768px]:h-auto bg-white shadow-lg border-2 hover:border-[#00befe] p-4 rounded-xl hover:scale-[0.98] cursor-pointer transition-all'>
-                <img src={house} alt="" className='max-[768px]:w-full max-[768px]:h-[200px] w-[160px] h-full rounded-xl bg-auto bg-no-repeat bg-center'/>
+                <img src={house} alt="" className='max-[768px]:w-full max-[768px]:h-[160px] w-[160px] h-full rounded-xl bg-auto bg-no-repeat bg-center'/>
                 <div className='flex flex-col max-[768px]:gap-4 gap-7 w-full h-full'>
                     <div className='flex flex-col gap-3'>
                         <h3 className='font-bold text-2xl'>103/143 West Street, <br/>Crows Nest</h3>
