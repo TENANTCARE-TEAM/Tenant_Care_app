438b3bac3268dc5b2e137482d55abd8f0c3353f4
yaasiinaxmed <yaskassoy@gmail.com>
2023-07-25 12:07:46 +0300

    console

--- src/components/TenDhashContent/ApprContent.jsx
+++ src/components/TenDhashContent/ApprContent.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useGetAprrovedRequestQuery } from "../../store/api/ItemsSlice";
 
 function ApprContent() {
+  
+  const {data: Approved = [], error} = useGetAprrovedRequestQuery()
+
+  console.log("API Tenant approved properties: ", Approved)
+  console.log("API Tenant approved properties Error:", error)
+  
   return (
     <div className="mt-5 bg-white p-6 w-full flex flex-col shadow rounded">
-        {/* title */}
+      {/* title */}
       <div className="flex">
         <h3 className="font-bold text-2xl">Approved Properties</h3>
       </div>
-    {/* Content  */}
-    
-        </div>
-  )
+      {/* Content  */}
+    </div>
+  );
 }
 
-export default ApprContent
+export default ApprContent;
